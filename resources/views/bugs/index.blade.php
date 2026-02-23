7f7533e3c15445f09a63e415bc52eb5e5443b7a5
Kyle Quilty <kquilty@gmail.com>
2026-02-23 17:06:49 -0500

    Created /bugs landing page

---
+++ resources/views/bugs/index.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bug Tracker - Bugs</title>
+</head>
+<body>
+    <h1>Existing Bugs</h1>
+    <ul>
+        <li>Bug 1: Application crashes on login.</li>
+        <li>Bug 2: Incorrect total displayed in shopping cart.</li>
+        <li>Bug 3: Profile picture upload fails.</li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bugs', function () {
+    return view('bugs.index');
+});
