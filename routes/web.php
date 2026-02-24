2a2945ac3f986b42a1abd833493feb3d0f0345e7
Kyle Quilty <kquilty@gmail.com>
2026-02-24 17:08:52 -0500

    Some Cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BugController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/bugs', [App\Http\Controllers\BugController::class, 'index']);
-
-Route::get('/bugs/report', [App\Http\Controllers\BugController::class, 'report']);
-
-Route::get('/bugs/{id}', [App\Http\Controllers\BugController::class, 'show']);
+Route::get('/bugs',         [BugController::class, 'index']);
+Route::get('/bugs/report',  [BugController::class, 'report']);
+Route::get('/bugs/{id}',    [BugController::class, 'show']);
